2bc758b1e85174ee07de6bda86fee822167af832
GabrielFelix23 <srgfelix123@gmail.com>
2021-01-08 15:45:22 -0300

    Exibindo as tarefas atrasadas

--- WEB/src/components/Header/styles.js
+++ WEB/src/components/Header/styles.js
@@ -32,7 +32,13 @@ export const RightSide = styled.div`
     align-items: center;
     justify-content: flex-end;
 
-    a{
+    button{
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+
+    a, button{
         color: #fff;
         font-weight: bold;
         text-decoration: none;
