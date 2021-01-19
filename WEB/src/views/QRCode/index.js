71e89235a2ff958ac7b42af4174dc1ef3c15c525
GabrielFelix23 <srgfelix123@gmail.com>
2021-01-19 20:08:22 -0300

    Gerando o QrCode

--- WEB/src/views/QRCode/index.js
+++ WEB/src/views/QRCode/index.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect}  from 'react'
 import * as S from './styles'
+import Qr from 'qrcode-react'
 
 //Nossos components
 import Header from '../../components/Header'
@@ -11,10 +12,16 @@ function QrCode() {
           <Header/>
             <S.Containt>
                 <h1>CAPTURE O QRCODE PELO APP</h1>
-                <S.QrCodeArea>
-                    
-                </S.QrCodeArea>
                 <p>Suas atividades serão sincronizadas com a do seu celular.</p>
+                <S.QrCodeArea>
+                    <Qr value="getmacaddress" size={250}/>
+                </S.QrCodeArea>
+
+                <S.ValidationCode>
+                  <span>Digite a numeração que apareceu no seu celular</span>
+                  <input type="text"/>
+                  <button type="button">Sincronizar</button>
+                </S.ValidationCode>
             </S.Containt>
           <Footer/>
       </S.Container>
